b1957189e058fbb180e0c205fdd90f821f1788f8
hwoo1211 <34612312+hwoo1211@users.noreply.github.com>
2020-10-14 19:19:30 -0500

    Button press implemented

---
+++ Project 2/Assets/Scenes/PlayMode/Scripts/ButtonHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+    private SpriteRenderer spRen;
+
+    public Sprite defaultButton;
+    public Sprite pressedButton;
+    public KeyCode pressKey;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spRen = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(pressKey))
+        {
+            spRen.sprite = pressedButton;
+        }
+        if (Input.GetKeyUp(pressKey))
+        {
+            spRen.sprite = defaultButton;
+        }
+
+    }
+}
