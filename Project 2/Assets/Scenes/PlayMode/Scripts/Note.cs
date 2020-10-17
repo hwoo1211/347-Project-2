b97dac029ef83eba01472405acc4a38087a8f92e
hwoo1211 <34612312+hwoo1211@users.noreply.github.com>
2020-10-16 20:34:48 -0500

    implementation of notes falling down according to the bpm.

---
+++ Project 2/Assets/Scenes/PlayMode/Scripts/Note.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour
+{
+    public Transform[] points;
+    public float beat;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //beat = 60f / 130f * 2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        this.transform.position -= new Vector3(0.0f, beat * Time.deltaTime, 0.0f);
+
+        //Vector3 compVec = new Vector3(0.0f, -8.0f, 0.0f);
+
+        if (this.transform.position.y <= -8)
+            Destroy(this);
+    }
+
+    public void setBeat(float bt)
+    {
+        beat = bt;
+    }
+
+    float getBeat()
+    {
+        return beat;
+    }
+}
---
+++ Project 2/Assets/Scenes/PlayMode/Scripts/Spawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] notes;
+    public Transform[] points;
+    public Transform[] HitBoxes;
+    public float beat;
+    public float timer;
+
+    private GameObject note;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        beat = (60f/130f * 2f);
+        note = new GameObject();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int idx = Random.Range(0, 4);
+        if (timer > beat)
+        {
+            note.AddComponent<Note>();
+            note.GetComponent<Note>().setBeat(beat);
+            note = Instantiate(notes[idx], points[idx]);         
+            note.transform.position = new Vector3(points[idx].position.x, points[idx].position.y, 2.0f);
+
+            timer -= beat;
+        }
+        timer += Time.deltaTime;
+    }
+
+    public void setBeat(float bt)
+    {
+        beat = bt;
+    }
+}
+
