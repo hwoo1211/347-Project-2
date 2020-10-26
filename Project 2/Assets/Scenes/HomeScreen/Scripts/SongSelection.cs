9f8bddddff1829c8c051efc59b3797a21463746f
Demola Ayokanmbi <aoa0006@uah.edu>
2020-10-25 22:45:35 -0500

    Home Screen UI Implemented

---
+++ Project 2/Assets/Scenes/HomeScreen/Scripts/SongSelection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class SongSelection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void PlayGame()
+    {
+        // Suggestion: Use SceneManager.MoveGameObjectToScene() to add song info to PlayMode Scene
+        SceneManager.LoadScene("PlayMode");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
